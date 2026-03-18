2c0e9ac0c8909bff61af52cf4d69041179248494
devcaetan0 <davi_caetano@estudante.sesisenai.org.br>
2026-03-18 08:20:04 -0300

    feat - implementação da função de inverter o sinal dos números

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ function add_char(caractere) {
     document.querySelector('.display').value = valor_input + caractere;
 }
 
-function limpa_tela(){
+function limpa_tela() {
     document.querySelector('.display').value = '';
+}
+
+function calcula() {
+    const valor_input = document.querySelector('.display').value;
+
+    document.querySelector('.display').value = eval(valor_input);
+}
+
+function inverte_sinal() {
+    const valor_input = document.querySelector('.display').value;
+
+    document.querySelector('.display').value = valor_input * -1;
 }
